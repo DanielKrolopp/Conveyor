0999968b6513c4a55b1e4632f179c9dcbfaacac8
JordanR159 <reed226@purdue.edu>
2020-04-29 12:59:22 -0400

    Fix issues with double appending components

--- examples/connected_components_parallel.py
+++ examples/connected_components_parallel.py
@@ -8,6 +8,8 @@ class ConnectedComponent:
     def __init__(self):
         self.partition = []
         self.union_find = []
+        self.count = 0
+        self.processors = 8
 
         def grow_spanning_tree(current_component, neighbors, usable_nodes):
             for neighbor in neighbors:
@@ -28,28 +30,27 @@ class ConnectedComponent:
                 for node in self.partition:
                     usable_nodes.append(node.get("vertex"))
                 components = []
-                current_component = []
                 total_nodes = len(self.partition)
                 count = 0
                 while count < total_nodes:
+                    current_component = []
                     current_node = self.partition.pop()
                     usable_nodes.remove(current_node.get("vertex"))
                     current_component.append(current_node.get("vertex"))
                     grow_spanning_tree(current_component, current_node.get("neighbors"), usable_nodes)
                     components.append(current_component)
                     count += len(current_component)
-                components.append([])
                 return components
 
         def merge_connected_component(arg):
-            if arg:
-                for component in arg:
-                    root = component[0]
-                    for node in component:
-                        while node >= len(self.union_find):
-                            self.union_find.append(len(self.union_find))
-                        self.union_find[node] = self.union_find[root]
-            else:
+            for component in arg:
+                root = component[0]
+                for node in component:
+                    while node >= len(self.union_find):
+                        self.union_find.append(len(self.union_find))
+                    self.union_find[node] = self.union_find[root]
+            self.count += 1
+            if self.count == self.processors:
                 components = []
                 for i in range(len(self.union_find)):
                     components.append([])
@@ -71,9 +72,9 @@ class ConnectedComponent:
             return graph
 
         pl = PdpPipeline.PdpPipeline()
-        pl.add(PdpStages.PdpBalancingFork(8))
+        pl.add(PdpStages.PdpBalancingFork(self.processors))
         pl.add(PdpStages.PdpProcessor(create_spanning_tree))
-        pl.add(PdpStages.PdpJoin(8))
+        pl.add(PdpStages.PdpJoin(self.processors))
         pl.add(PdpStages.PdpProcessor(merge_connected_component))
 
         graph = create_input()
