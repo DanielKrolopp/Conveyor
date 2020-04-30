8462a86503a05e398f7dbb360ffac0240f014d4e
Daniel Krolopp <dkrolopp@purdue.edu>
2020-04-30 02:26:04 -0400

    Name change in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="conveyor",
+    name="parallel-conveyor",
     version="0.0.1",
     author="Conveyor Team",
     description="Simple, intuitive pipelining in Python",
