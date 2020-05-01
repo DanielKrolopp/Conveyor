d5ee3108e8f5bb4855937260a087c47b1cb38378
Aaron Neustedter <aneusted@purdue.edu>
2020-04-30 21:19:59 -0400

    Version bump to 3.8 for shared_memory module

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="parallel-conveyor",
-    version="0.0.1",
+    version="0.0.2",
     author="Conveyor Team",
     description="Simple, intuitive pipelining in Python",
     long_description=long_description,
@@ -18,5 +18,5 @@ setuptools.setup(
     ],
     test_suite='nose.collector',
     tests_require=['nose'],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
 )
